fc9b9f75c86eb49c15d33cb0124636d07ba077b6
Rubén Peregrina Aroca <peregrinaruben@gmail.com>
2023-02-09 15:16:59 +0100

    Refactor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home/home.component';
-import { ContactComponent } from './contact/contact/contact.component';
-import { AboutComponent } from './about/about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about/about.component';
-import { ContactComponent } from './contact/contact/contact.component';
-import { HomeComponent } from './home/home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
