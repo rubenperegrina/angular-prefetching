3ef1f27cea9e2a0b8db7f012f6f18202ce9a17ad
Rubén Peregrina Aroca <peregrinaruben@gmail.com>
2023-02-09 15:08:59 +0100

    Init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home/home.component';
+import { ContactComponent } from './contact/contact/contact.component';
+import { AboutComponent } from './about/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+    title: 'Home'
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+    title: 'Contact'
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+    title: 'About'
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about/about.component';
+import { ContactComponent } from './contact/contact/contact.component';
+import { HomeComponent } from './home/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
